6a30a10f6bd392eed23e792e062e73f2b1448052
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2014-04-10 19:40:09 +0200

    Split into subprojects.

--- webapp/src/main/java/info/rnowak/springFun/controller/HelloController.java
+++ webapp/src/main/java/info/rnowak/springFun/controller/HelloController.java
@@ -1,4 +1,4 @@
-package info.rnowak.springFun.web;
+package info.rnowak.springFun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
